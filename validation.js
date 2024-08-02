40189cd6cef5f505ca08cc2bb9fc9f64d8dc2bb2
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-08-02 23:03:19 +0300

    password and password check validation

--- validation.js
+++ validation.js
@@ -1,20 +1,3 @@
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  formControl.className = 'form-control success';
-  small.style.visibility = 'hidden';
-  console.log('success');
-}
-
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  formControl.className = 'form-control error';
-  small.innerText = message;
-  small.style.visibility = 'visible';
-  console.log('errrrrorr');
-}
-
 const form = document.querySelector('#form');
 const username = document.querySelector('#username');
 const email = document.querySelector('#email');
@@ -26,15 +9,14 @@ form.addEventListener('submit', validationHeandler);
 function validationHeandler(event) {
   event.preventDefault();
 
-  console.log('handler works');
   checkInputs();
 }
 
 function checkInputs() {
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
-  // const passwordValue = password.value.trim();
-  // const password2Value = password2.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
 
   if (usernameValue === '') {
     setErrorFor(username, 'Username cannot be blank');
@@ -47,4 +29,33 @@ function checkInputs() {
   } else {
     setSuccessFor(email);
   }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === '') {
+    setErrorFor(password2, 'Password cannot be blank');
+  } else if (password2Value !== passwordValue) {
+    setErrorFor(password2, "Password doesn't match");
+  } else {
+    setSuccessFor(password2);
+  }
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  formControl.className = 'form-control success';
+  small.style.visibility = 'hidden';
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  formControl.className = 'form-control error';
+  small.innerText = message;
+  small.style.visibility = 'visible';
 }
